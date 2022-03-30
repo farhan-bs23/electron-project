f97f950d40ac8290625c207d5fede4de091d2474
Farhan Mohd. Fokrul Alam <farhan.mohd@brainstation-23.com>
2022-03-30 12:07:23 +0600

    add playlist element listeners in renderer

--- src/renderer.ts
+++ src/renderer.ts
@@ -4,3 +4,32 @@
 // nodeIntegration is set to true in webPreferences.
 // Use preload.js to selectively enable features
 // needed in the renderer process.
+
+//DOM element references
+const showModal = document.getElementById("show-modal"),
+    closeModal = document.getElementById("close-modal"),
+    addItem = document.getElementById("add-item"),
+    inputUrlElem = document.getElementById("url") as HTMLInputElement,
+    modal = document.getElementById("modal");
+
+//show modal
+showModal.addEventListener("click", () => {
+    modal.style.display = "flex";
+    inputUrlElem.focus();
+});
+
+//hide modal
+closeModal.addEventListener("click", () => {
+    modal.style.display = "none";
+});
+
+//add to list
+addItem.addEventListener("click", () => {
+    console.log(inputUrlElem.value);
+});
+
+inputUrlElem.addEventListener("keyup", (event) => {
+    if (event.key === "Enter") {
+        addItem.click();
+    }
+});
